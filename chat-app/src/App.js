193b8a627232fc17804548d42110291fbecc157f
Chitsanupong <first30952@gmail.com>
2021-10-10 14:03:48 +0700

    feat: add chat engine component

--- chat-app/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <div>React App</div>
-    </>
-  );
-}
-
-export default App;
---
+++ chat-app/src/App.jsx
@@ -0,0 +1,19 @@
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={process.env.REACT_APP_PROJECT_ID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
+  );
+}
+
+export default App;
